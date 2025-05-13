0313ea825ab57d1c55e586b5a20621705a28cfaf
Facundo <facuaguilera32@gmail.com>
2025-05-13 15:01:50 -0300

    adding product controller

---
+++ src/main/java/com/group_three/food_ordering/controllers/ProductController.java
@@ -0,0 +1,49 @@
+package com.group_three.food_ordering.controllers;
+
+import com.group_three.food_ordering.configs.ApiPaths;
+import com.group_three.food_ordering.dtos.create.ProductCreateDto;
+import com.group_three.food_ordering.dtos.response.ProductResponseDto;
+import com.group_three.food_ordering.dtos.update.ProductUpdateDto;
+import com.group_three.food_ordering.models.Product;
+import com.group_three.food_ordering.services.interfaces.IProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ApiPaths.PRODUCT_BASE)
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final IProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(
+            @RequestBody @Valid ProductCreateDto productCreateDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.create(productCreateDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getProducts()
+    {
+        return ResponseEntity.ok(productService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getById(id));
+    }
+/*
+    @PostMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductUpdateDto productUpdateDto)
+    {
+        return ResponseEntity.ok(productService.update(id));
+    }
+*/
+
+
+}
