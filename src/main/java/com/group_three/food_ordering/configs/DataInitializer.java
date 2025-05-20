32e04ebaa07b2773707010c59cc03789841425c5
RodriLang <140997139+RodriLang@users.noreply.github.com>
2025-05-19 21:31:55 -0300

    Update DataInitializer.java

--- src/main/java/com/group_three/food_ordering/configs/DataInitializer.java
+++ src/main/java/com/group_three/food_ordering/configs/DataInitializer.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
-
+ 
     private final ICategoryRepository categoryRepository;
     private final ITagRepository tagRepository;
     private final IFoodVenueRepository foodVenueRepository;
