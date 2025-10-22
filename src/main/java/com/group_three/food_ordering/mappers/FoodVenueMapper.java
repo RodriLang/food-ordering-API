92f95ac35adf1da885ad3d8c2040698e170d92fc
Ezequiel Santalla <ezequielasantalla@gmail.com>
2025-10-22 20:46:10 -0300

    food venue mapper fixed

--- src/main/java/com/group_three/food_ordering/mappers/FoodVenueMapper.java
+++ src/main/java/com/group_three/food_ordering/mappers/FoodVenueMapper.java
@@ -7,17 +7,22 @@ import com.group_three.food_ordering.models.FoodVenue;
 import com.group_three.food_ordering.dto.response.FoodVenuePublicResponseDto;
 import org.mapstruct.*;
 
-@Mapper(componentModel = "spring", uses = {AddressMapper.class})
+@Mapper(componentModel = "spring", uses = {AddressMapper.class, VenueStyleMapper.class, DiningTableMapper.class})
 public interface FoodVenueMapper {
 
+    @Mapping(target = "venueStyle", source = "styleRequestDto")
+    @Mapping(target = "diningTables", source = "tables")
     FoodVenue toEntity(FoodVenueRequestDto foodVenueDto);
 
     FoodVenueAdminResponseDto toAdminDto(FoodVenue foodVenue);
 
+    @Mapping(target = "styles", source = "venueStyle")
     @Mapping(source = "address", target = "address", qualifiedByName = "flatAddress")
     FoodVenuePublicResponseDto toPublicDto(FoodVenue foodVenue);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "venueStyle", source = "styleRequestDto")
+    @Mapping(target = "diningTables", source = "tables")
     void updateEntity(FoodVenueRequestDto dto, @MappingTarget FoodVenue entity);
 
     @Named("flatAddress")
@@ -25,4 +30,3 @@ public interface FoodVenueMapper {
         return address.getStreet() + " " + address.getNumber();
     }
 }
-
