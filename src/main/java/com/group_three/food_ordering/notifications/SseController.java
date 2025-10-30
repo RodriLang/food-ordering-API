185dda3bfe59b3d4750402d05d279bc707625f47
rodrilang <rodrigolang90mail.com>
2025-10-30 20:53:14 -0300

    refactorizacion de packages

--- src/main/java/com/group_three/food_ordering/notifications/SseController.java
+++ src/main/java/com/group_three/food_ordering/notifications/SseController.java
@@ -1,6 +1,6 @@
 package com.group_three.food_ordering.notifications;
 
-import com.group_three.food_ordering.configs.ApiPaths;
+import com.group_three.food_ordering.utils.constants.ApiPaths;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +14,6 @@ public class SseController {
 
     private final SseService sseService;
 
-    // MUY IMPORTANTE: Configurar CORS para permitir la conexión desde tu Angular
-    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/table-sessions/{tableSessionId}")
     public SseEmitter subscribeToTableSession(@PathVariable String tableSessionId) {
         // Llama al servicio para crear y guardar el emitter
